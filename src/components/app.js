690e824fd27e8b595d6a07e1f18a019432f929a3
Ryan Delk <ryan@thedelk.com>
2018-11-17 18:35:03 -0600

    [optimize] add "position" to list items to prevent map flash on marker click

--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,95 @@
 import React, { Component } from "react";
 import "../styles/app.css";
+import { getPlacesData } from "../util/requests";
 import MapContainer from "./map";
+import Sidebar from "./sidebar";
 
 export default class App extends Component {
   state = {
+    placeSelected: null,
     mapCenter: {
       lat: 35.465076,
       lng: -97.507373
     },
-    mapMarkerList: [],
     mapZoom: 12,
-    placeList: [],
-    placeSelected: null
+    mapMarkerInfoWindowShowing: false,
+    mapMarkerList: [],
+    mapMarkerSelected: {},
+    placeDetails: {},
+    placeList: []
+  };
+
+  componentDidMount() {
+    this.getPlaces();
+  }
+
+  getPlaces = () => {
+    getPlacesData()
+      .then(response => {
+        this.setState({
+          placeList: response.response.groups[0].items
+        });
+
+        let test = this.state.placeList.map(place => {
+          return {
+            ...place,
+            position: {
+              lat: place.venue.location.lat,
+              lng: place.venue.location.lng
+            }
+          };
+        });
+
+        this.setState({
+          placeList: test
+        });
+      })
+      .catch(error => alert(`Error: ${error}`));
+  };
+
+  markerWasSelected = (props, marker) => {
+    this.setState({
+      placeDetails: {},
+      placeSelected: props,
+      mapMarkerSelected: marker,
+      mapMarkerInfoWindowShowing: true
+    });
   };
 
   render() {
+    console.log("app render");
+    console.log(this);
+
     const {
       mapCenter,
+      mapMarkerInfoWindowShowing,
       mapMarkerList,
+      mapMarkerSelected,
       mapZoom,
+      placeDetails,
       placeList,
       placeSelected
     } = this.state;
 
     return (
-      <div className="col col-map">
-        <MapContainer
-          mapCenter={mapCenter}
-          mapMarkerList={mapMarkerList}
-          mapZoom={mapZoom}
-          placeList={placeList}
-          placeSelected={placeSelected}
-        />
+      <div role="application">
+        <main className="container">
+          <Sidebar />
+          <div className="col-map">
+            <MapContainer
+              mapMarkerList={mapMarkerList}
+              placeList={placeList}
+              markerWasSelected={this.markerWasSelected}
+              mapMarkerSelected={mapMarkerSelected}
+              mapZoom={mapZoom}
+              markerRef={this.getMarkerRef}
+              mapMarkerInfoWindowShowing={mapMarkerInfoWindowShowing}
+              placeDetails={placeDetails}
+              mapCenter={mapCenter}
+              placeSelected={placeSelected}
+            />
+          </div>
+        </main>
       </div>
     );
   }
--- src/components/map.js
+++ src/components/map.js
@@ -1,34 +1,89 @@
-import React, { Component } from "react";
+import React from "react";
 import { Map, Marker, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 import { G_KEY } from "../util/auth.js";
+import theme from "../styles/theme";
 
-export class MapContainer extends Component {
-  render() {
-    const {
-      mapCenter,
-      mapMarkerList,
-      mapZoom,
-      placesList,
-      placeSelected
-    } = this.props;
+const MapContainer = props => {
+  console.log("in mapContainer");
+  console.log(props);
 
+  const {
+    google,
+    mapCenter,
+    mapMarkerInfoWindowShowing,
+    mapMarkerList,
+    mapMarkerSelected,
+    mapZoom,
+    markerWasSelected,
+    markerRef,
+    placeList,
+    placeSelected
+  } = props;
+
+  const markers = placeList.map(place => {
     return (
-      <Map
-        className="map"
-        google={this.props.google}
-        onClick={this.onMapClick}
-        style={{
-          position: "relative",
-          display: "inherit",
-          height: "100%",
-          width: "100%"
-        }}
-        zoom={mapZoom}
-        initialCenter={mapCenter}
+      <Marker
+        className="marker"
+        key={place.venue.id}
+        position={place.position}
+        onClick={props.markerWasSelected.bind(this)}
+        name={place.name}
       />
     );
-  }
-}
+  });
+
+  return (
+    <Map
+      google={google}
+      onready={console.log("render map.js")}
+      // onClick={this.onMapClick}
+      style={{
+        // position: "relative",
+        // display: "inherit",
+        height: "100%",
+        width: "100%"
+      }}
+      zoom={mapZoom}
+      initialCenter={mapCenter}
+      styles={theme}
+      disableDefaultUI={true}
+    >
+      {console.log(props)}
+      {markers}
+      {/* {placeList.map(place => {
+        console.log(this);
+        return (
+          <Marker
+            className="marker"
+            // key={place.venue.id}
+            key={place.name}
+            // id={place.venue.id}
+            onClick={markerWasSelected.bind(this)}
+            // onClick={console.log("refresh?")}
+            name={place.name}
+            // position={{
+            //   lat: place.venue.location.lat,
+            //   lng: place.venue.location.lng
+            // }}
+            position={{
+              lat: place.position.lat,
+              lng: place.position.lng
+            }}
+          />
+        );
+      })} */}
+      <InfoWindow
+        visible={mapMarkerInfoWindowShowing}
+        marker={mapMarkerSelected}
+      >
+        <section>
+          <div>Title</div>
+        </section>
+      </InfoWindow>
+    </Map>
+  );
+};
+// }
 
 export default GoogleApiWrapper({
   apiKey: G_KEY
