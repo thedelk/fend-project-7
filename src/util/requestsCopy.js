5738cf915d75f0276c7d4befe10839a348e08a33
Ryan Delk <ryan@thedelk.com>
2018-11-25 22:01:45 -0600

    [start] begin writing function logic for retrieving place detail information

--- src/util/requestsCopy.js
+++ src/util/requestsCopy.js
@@ -16,67 +16,73 @@ const PARAM_FS_SECRET = `&client_secret=${FS_SECRET}`;
 const URL_SEARCH = `${BASE_URL}search${PARAM_FS_ID}${PARAM_FS_SECRET}`;
 let id = "";
 const URL_DATA = `${BASE_URL}${id}v=${QUERY_VERSION}&`;
+
+const QUERY_TEST = {
+  test: "testValue",
+  ryan: "delk"
+};
+
 const CREDENTIALS = {
   client_id: FS_ID,
   client_secret: FS_SECRET
 };
-const QUERY = {
-  query: QUERY_TERM,
+
+const QUERY_PARAMS = {
   near: QUERY_LOCATION,
+  query: QUERY_TERM,
   v: QUERY_VERSION
 };
 
+const queryParamsString = Object.keys(QUERY_PARAMS)
+  .map(key => `${key}=${QUERY_PARAMS[key]}`)
+  .join("&")
+  .replace(" ", "_");
+
+//
+//
+// Classes
 class Helper {
-  static async getResponse(param) {
-    return await axios.get(this.getUrl(param)).then(response => {
+  static async getResponse() {
+    const args = [...arguments];
+    return await axios.get(this.getUrl([args])).then(response => {
       return response.data;
     });
   }
 
-  static getUrl(param) {
-    let query = new URLSearchParams(QUERY);
-    let creds = new URLSearchParams(CREDENTIALS);
+  static getUrl() {
+    console.log([...arguments][0][0]);
+    let theseArgs = [...arguments][0][0];
+    console.log(theseArgs);
+
+    // let testing = theseArgs.map(arg => ({}))
+
+    // console.log(theseArgs);
+    // let prms = new URLSearchParams(theseArgs);
+    // let prmsAll = prms.entries();
+    // console.log(prmsAll);
+    // console.log(...arguments);
+    // let query = new URLSearchParams(QUERY_PARAMS);
+    // let creds = new URLSearchParams(CREDENTIALS);
+    // let args = [...arguments][0].map(() => return arg);
+    // let allArgs = new URLSearchParams([...arguments]);
+    // console.log(args);
+
+    // console.log(args.toString());
 
     // 4fc4db63e4b0d9d21ed8cae2
 
     // return `${BASE_URL}${param}${query}&${creds}`;
-    return `${BASE_URL}${param}${query}&${creds}`;
+    return `https://api.foursquare.com/v2/venues/search?query=coffee&near=Oklahoma+City&v=20182507&client_id=XATAV0HZ0MUVRNP2DMR4202KKJVBPO4PPHI21YNDSNKBSXUC&client_secret=HTTEZQMQKMOGQXK1PWLOC3UTZI3HTWLULBMGZG5RY5T4UH1X`;
   }
-
-  // static async getResponseOld() {
-  //   const PARAMS = {
-  //     query: "coffee",
-  //     near: "Oklahoma City",
-  //     v: "20182507",
-  //     client_id: "XATAV0HZ0MUVRNP2DMR4202KKJVBPO4PPHI21YNDSNKBSXUC",
-  //     client_secret: "HTTEZQMQKMOGQXK1PWLOC3UTZI3HTWLULBMGZG5RY5T4UH1X"
-  //   };
-  //   return await axios
-  //     .get(ENDPOINT + new URLSearchParams(PARAMS))
-  //     .then(response => {
-  //       console.log(response);
-  //       return response.data;
-  //     })
-  //     .catch(error => {
-  //       console.log(
-  //         "Error fetching response from FOURSQUARE in getResponse() [requests.js]"
-  //       );
-  //       console.log(error);
-  //       return error;
-  //     });
-  // }
 }
 
 export default class RequestCopy {
   static getPlaces() {
-    return Helper.getResponse("search?");
+    // return Helper.getResponse("search?");
+    return Helper.getResponse(QUERY_PARAMS, CREDENTIALS);
   }
 
   static getPlacesData(id) {
     return Helper.getResponse(`${id}?`);
   }
-
-  // static searchVenues(params) {
-  //   return Helper.getResponseOld("/search", params);
-  // }
 }
