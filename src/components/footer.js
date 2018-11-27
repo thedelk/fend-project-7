1f51866ecd84a1da46410cc7c24de64dd391095a
Ryan Delk <ryan@thedelk.com>
2018-11-27 17:17:50 -0600

    [add] change "header" to "footer" and add Foursquare attribution

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+// @Author:             Ryan Delk
+// @Date:               2018-11-25 14:12:49
+// @Last Modified by:   Ryan Delk
+// @Last Modified time: 2018-11-25 14:12:49
+
+import React from "react";
+import "../styles/css/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h4>
+        Powered by <a href="https://developer.foursquare.com">Foursquare</a>
+      </h4>
+    </footer>
+  );
+};
+
+export default Footer;
